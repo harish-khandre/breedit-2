b35276045990ff772a6b0a1e879ec111415d7c62
haris-khandre <harishkhandre2901@gmail.com>
2024-05-20 01:38:34 +0530

    8: react email

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,28 +1,26 @@
+import { TwoFA } from "@/emails/2fa";
+import ResetPasswordEmail from "@/emails/reset-password";
+import { VerificationEmail } from "@/emails/verification-email";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@breedit.co.in",
     to: email,
     subject: "Your two-factor authentication code",
-    html: `
-      <p>Your two-factor authentication code is: ${token}</p>
-    `,
+    react: TwoFA({ token }),
   });
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const resetUrl = `${process.env.NEXT_PUBLIC_APP_URL}/auth/new-password?token=${token}`;
-
+  const url = `${process.env.NEXT_PUBLIC_APP_URL}/auth/new-password?token=${token}`;
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@breedit.co.in",
     to: email,
     subject: "Reset your password",
-    html: `
-      <p>Click <a href="${resetUrl}">here</a> to reset your password</p>
-    `,
+    react: ResetPasswordEmail({ url }),
   });
 };
 
@@ -30,12 +28,9 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmationUrl = `${process.env.NEXT_PUBLIC_APP_URL}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@breedit.co.in",
     to: email,
     subject: "Verify your email address",
-    html: `
-      <p>Click <a href="${confirmationUrl}">here</a> to verify your email address</p>
-      
-    `,
+    react: VerificationEmail({ confirmationUrl }),
   });
 };
