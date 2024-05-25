a6537501d5ce8750bace3da92f8cfbfebe5160b4
haris-khandre <harishkhandre2901@gmail.com>
2024-05-25 22:27:49 +0530

    para

--- components/waiting-list.tsx
+++ components/waiting-list.tsx
@@ -10,8 +10,10 @@ export function BackgroundBeamsDemo() {
         <h1 className=" py-8 relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-orange-200 to-orange-600  text-center font-sans font-bold">
           Work in progress
         </h1>
-        <p>Please check-in after sometime</p>
-        <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10"></p>
+        <p></p>
+        <p className="text-primary max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+          Please check-in after sometime
+        </p>
       </div>
       <BackgroundBeams />
     </div>
